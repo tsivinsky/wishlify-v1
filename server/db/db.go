7b5b50d6c2f105cb64753e8e3ad8da5468c013e5
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-01-24 11:47:18 +0300

    feat: add postgres

---
+++ server/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	Db gorm.DB
+)
+
+func Start() error {
+	host := os.Getenv("DATABASE_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	name := os.Getenv("POSTGRES_DB")
+	port := "5432"
+
+	uri := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow", host, user, password, name, port)
+
+	Db, err := gorm.Open(postgres.Open(uri))
+	if err != nil {
+		return err
+	}
+
+	err = Db.AutoMigrate()
+
+	return err
+}
--- server/main.go
+++ server/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+	"wishlify/db"
 	"wishlify/router"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	err := db.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
 
 	router.Start(app)

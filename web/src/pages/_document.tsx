b8b42c64e25c7ba3dbf8284a34b8e2bef64356ad
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-01-27 09:43:29 +0300

    feat(web): add vercel favicon

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />

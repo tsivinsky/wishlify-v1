18c2bac017478338522642cce24d35543a828b8a
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-01-29 13:52:35 +0300

    feat(backend): add `ConfirmedAt` field to user model

--- server/db/user.go
+++ server/db/user.go
@@ -1,9 +1,12 @@
 package db
 
+import "database/sql"
+
 type User struct {
 	Model
 
-	Email    string `json:"email" gorm:"unique"`
-	Username string `json:"username" gorm:"unique"`
-	Password string `json:"password"`
+	Email       string       `json:"email" gorm:"unique"`
+	Username    string       `json:"username" gorm:"unique"`
+	Password    string       `json:"password"`
+	ConfirmedAt sql.NullTime `json:"confirmedAt"`
 }

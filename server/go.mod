d7636194ac383beef20958c97e866b8a230971f2
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-01-29 17:45:57 +0300

    fix(backend): add package for sending emails

--- server/go.mod
+++ server/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.1
 	github.com/gofiber/fiber/v2 v2.41.0
 	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/xhit/go-simple-mail/v2 v2.13.0
 	golang.org/x/crypto v0.5.0
 	gorm.io/driver/postgres v1.4.6
 	gorm.io/gorm v1.24.3
@@ -15,6 +16,7 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-test/deep v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.2.0 // indirect
@@ -26,6 +28,7 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- server/lib/email.go
+++ server/lib/email.go
@@ -3,74 +3,54 @@ package lib
 import (
 	"crypto/tls"
 	"fmt"
-	"net/smtp"
 	"os"
+	"strconv"
+	"time"
+
+	mail "github.com/xhit/go-simple-mail/v2"
 )
 
 func SendEmail(to string, subject string, msg string, useHTML bool) error {
 	from := os.Getenv("EMAIL_USER")
 	password := os.Getenv("EMAIL_PASSWORD")
 	host := os.Getenv("EMAIL_HOST")
-	port := os.Getenv("EMAIL_PORT")
+	portStr := os.Getenv("EMAIL_PORT")
 
-	addr := fmt.Sprintf("%s:%s", host, port)
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return err
+	}
 
-	contentType := "text/plain"
+	server := mail.NewSMTPClient()
 
+	server.Host = host
+	server.Port = port
+	server.Username = from
+	server.Password = password
+	server.Encryption = mail.EncryptionSSLTLS
+	server.KeepAlive = false
+	server.SendTimeout = 10 * time.Second
+	server.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	client, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+
+	contentType := mail.TextPlain
 	if useHTML {
-		contentType = "text/html"
+		contentType = mail.TextHTML
 	}
 
-	message := fmt.Sprintf(`From: Wishlify<%s>
-To: %s
-Subject: %s
-Content-Type: %s
+	email.SetFrom(fmt.Sprintf("Wishlify <%s>", from)).AddTo(to).SetSubject(subject).SetBody(contentType, msg)
 
-%s`, from, to, subject, contentType, msg)
-
-	auth := smtp.PlainAuth("Wishlify", from, password, host)
-
-	conn, err := tls.Dial("tcp", addr, &tls.Config{ServerName: host})
-	if err != nil {
+	if email.Error != nil {
 		return err
 	}
 
-	client, err := smtp.NewClient(conn, host)
-	if err != nil {
-		return err
-	}
-
-	err = client.Auth(auth)
-	if err != nil {
-		return err
-	}
-
-	err = client.Mail(from)
-	if err != nil {
-		return err
-	}
-
-	err = client.Rcpt(to)
-	if err != nil {
-		return err
-	}
-
-	w, err := client.Data()
-	if err != nil {
-		return err
-	}
-
-	_, err = w.Write([]byte(message))
-	if err != nil {
-		return err
-	}
-
-	err = w.Close()
-	if err != nil {
-		return err
-	}
-
-	err = client.Quit()
+	err = email.Send(client)
 	if err != nil {
 		return err
 	}
